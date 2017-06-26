82a51561f5a89c9b1c9594330db5fc84c4e08f71
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2017-06-26 10:51:10 +0300

    Restructure files and folders

--- src/containers/app/app.js
+++ src/containers/app/app.js
@@ -4,6 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="app">
+        <h1>Todux</h1>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './containers/app/app';
+import registerServiceWorker from './common/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
