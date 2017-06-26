36ee7645a723da5f2e672266b672e3255fac94c5
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2017-06-26 11:55:19 +0300

    Stable version

--- src/containers/app/app.js
+++ src/containers/app/app.js
@@ -1,13 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-class App extends Component {
+import { actionCreators } from './todoListRedux'
+import List from '../../components/list'
+import Input from '../../components/input'
+import Title from '../../components/title'
+
+export default class App extends Component {
+
+  state = {}
+
+  componentWillMount() {
+    const {store} = this.props
+
+    const {todos} = store.getState()
+    this.setState({todos})
+
+    this.unsubscribe = store.subscribe(() => {
+      const {todos} = store.getState()
+      this.setState({todos})
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe()
+  }
+
+  onAddTodo = (text) => {
+    const {store} = this.props
+
+    store.dispatch(actionCreators.add(text))
+  }
+
+  onRemoveTodo = (index) => {
+    const {store} = this.props
+
+    store.dispatch(actionCreators.remove(index))
+  }
+
   render() {
+    const {todos} = this.state
+
     return (
-      <div className="app">
-        <h1>Todux</h1>
+      <div style={styles.container}>
+        <Title>
+          To-Do List
+        </Title>
+        <Input
+          placeholder={'Type a todo, then hit enter!'}
+          onSubmitEditing={this.onAddTodo}
+        />
+        <List
+          list={todos}
+          onClickItem={this.onRemoveTodo}
+        />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/app/app';
-import registerServiceWorker from './common/registerServiceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Import the App container component
+import App from './containers/app/app'
+
+// Import the reducer and create a store
+import { reducer } from './containers/app/todoListRedux'
+
+const store = createStore(reducer);
+
+
+// Pass the store into the app container
+render(<App store={store} />, document.querySelector('#root'));
